49701c85961052865cf3488f5359e86bc67805de
Mehul Jain <mehuljain160@gmail.com>
2025-07-13 22:50:44 +0530

    create scripts folder

--- app/api_routes.py
+++ app/api_routes.py
@@ -4,4 +4,8 @@ from flask import Blueprint
 api_bp = Blueprint("api", __name__)
 
 # Import all controllers to register their routes
-from app.controllers.api.v1 import auth_controller, files_controller, users_controller  # noqa: F401, E402
+from app.controllers.api.v1 import (  # noqa: F401, E402
+    auth_controller,
+    files_controller,
+    users_controller,
+)
